3a744a344bb8954e34e0b74044323aedd784b026
Agusandika027 <igustiagusandika01@gmail.com>
2022-11-06 15:28:38 +0800

    Buttom Exit

--- src/PertambahanDuaAngkaGUI.java
+++ src/PertambahanDuaAngkaGUI.java
@@ -138,6 +138,11 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
 
         keluarBtn.setBackground(new java.awt.Color(255, 255, 153));
         keluarBtn.setText("Keluar");
+        keluarBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                keluarBtnActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -229,6 +234,11 @@ public class PertambahanDuaAngkaGUI extends javax.swing.JFrame {
     hasilTF.setText("");
     angkaPertamaTF.requestFocus();
     }//GEN-LAST:event_hapusBtnActionPerformed
+
+    private void keluarBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_keluarBtnActionPerformed
+        // TODO add your handling code here:
+                System.exit(0);
+    }//GEN-LAST:event_keluarBtnActionPerformed
 
     /**
      * @param args the command line arguments
